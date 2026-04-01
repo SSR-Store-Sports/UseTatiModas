9c425fc29bc56e0184859467982514f19fb0be50
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-04-01 20:05:44 -0300

    ✨ feat: Add styles in sign up page

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -13,48 +13,68 @@
         <span class="bg-gray-300 h-0.5 w-32"></span>
       </div>
 
-      <form action="/session" method="POST" class="flex flex-col w-full gap-4">
+      <form action="/session" method="POST" class="flex flex-col w-full gap-8">
         @csrf
 
         <div class="flex flex-col gap-4">
           <div class="flex flex-col gap-2 flex-1">
-            <span class="text-2xl">E-mail</span>
-            <input class="border-2 border-black rounded-sm pr-4 pl-4 pt-3 pb-3" type="email" name="email" id="email" placeholder="Henrrylimadasilva@gmail.com" />
+            <span class="text-lg">E-mail</span>
+            <input
+              class="w-full px-4 py-3 rounded-md border border-gray-200 bg-gray-50 text-gray-800 placeholder-gray-400 text-sm outline-none transition-all duration-200 hover:border-pink-400 hover:bg-white focus:border-pink-500 focus:bg-white focus:shadow-[0_0_0_3px_rgba(236,72,153,0.15)]"
+              type="email" name="email" id="email" placeholder="exemplo@email.com" />
           </div>
 
           <div class="flex flex-col gap-2 flex-1">
             <div class="flex justify-between">
-              <span class="text-2xl">Senha</span>
-              <a href="/forget/password">Esqueceu sua senha?</a>
+              <span class="text-lg">Senha</span>
+              <a href="/forget/password"
+                class="transition-all duration-200 text-blue-600 hover:to-blue-700 hover:underline ">Esqueceu sua
+                senha?</a>
             </div>
 
-            <label for="password" class="flex border-2 border-black rounded-sm px-4 py-3 items-center gap-2">
+            <label 
+              for="password"
+              class="flex w-full px-4 py-3 rounded-md border border-gray-200 bg-gray-50 text-gray-800 placeholder-gray-400 text-sm outline-none transition-all duration-200 hover:border-pink-400 hover:bg-white focus:border-pink-500 focus:bg-white focus:shadow-[0_0_0_3px_rgba(236,72,153,0.15)] text-center justify-center align-center"
+            >
               <input class="w-full outline-none" type="password" name="password" id="password" placeholder="Senha" />
               <img class="h-4 w-4" src="{{ asset('assets/eye_slash.png') }}" alt="Icon de olhos para senha." />
             </label>
           </div>
         </div>
 
-        <button class="bg-pink-500 text-white flex items-center justify-center rounded-sm w-full pt-3 pb-3 gap-2">
+        <button
+          class="group bg-pink-500 text-white flex items-center justify-center rounded-sm w-full pt-3 pb-3 gap-2 border-2 border-transparent hover:bg-white hover:border-2 hover:border-pink-600 hover:text-pink-600 cursor-pointer text-center">
           <span>Entrar</span>
-          <img 
+          <!-- <img class="h-4 w-4   group-hover:text-pink-600" src="{{ asset('assets/sign_in.png') }}"
+            alt="Icon representando uma porta realizando uma analogia de entrada ou acessando a aplicação" /> -->
+          <svg 
             class="h-4 w-4" 
-            src="{{ asset('assets/sign_in.png') }}" 
-            alt="Icon representando uma porta realizando uma analogia de entrada ou acessando a aplicação" />
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 24 24" 
+            fill="currentColor"
+            className="size-6">
+            <path 
+              fillRule="evenodd"
+              d="M16.5 3.75a1.5 1.5 0 0 1 1.5 1.5v13.5a1.5 1.5 0 0 1-1.5 1.5h-6a1.5 1.5 0 0 1-1.5-1.5V15a.75.75 0 0 0-1.5 0v3.75a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5.25a3 3 0 0 0-3-3h-6a3 3 0 0 0-3 3V9A.75.75 0 1 0 9 9V5.25a1.5 1.5 0 0 1 1.5-1.5h6ZM5.78 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 0 0 0 1.06l3 3a.75.75 0 0 0 1.06-1.06l-1.72-1.72H15a.75.75 0 0 0 0-1.5H4.06l1.72-1.72a.75.75 0 0 0 0-1.06Z"
+              clipRule="evenodd" 
+            />
+          </svg>
+
         </button>
       </form>
-      
+
       <span class="bg-gray-300 h-0.5 w-82"></span>
 
       <section class="flex flex-col gap-8 items-center justify-center">
-        <p>Não tem uma conta? <a href="/register">Cadastre-se</a></p>
+        <p>Não tem uma conta?  
+          <a href="/register" class="text-blue-600 hover:underline">
+            Cadastre-se
+          </a>
+        </p>
 
         <div class="flex flex-col gap-2 items-center justify-center">
-          <img 
-            class="h-4 w-4"
-            src="{{ asset('assets/help_me.png') }}" 
-            alt="sinal de interrogação para direcionar a tela de ajuda" 
-          />
+          <img class="h-4 w-4" src="{{ asset('assets/help_me.png') }}"
+            alt="sinal de interrogação para direcionar a tela de ajuda" />
           <a href="/help">Ajuda</a>
         </div>
       </section>
