0ab138900c00a949c835d7812e937686ebe7ecef
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-04-04 14:59:17 -0300

    ✨ feat: Add adjust in routes and styles of elements page

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('login.index');
 });
+
+Route::get('/register', function () {
+    return view('register.index');
+});
+
+Route::get('/', function () {
+    return view('index');
+});
+
 
 
 
