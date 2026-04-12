b98fa1b91e0bdff31d8a5934c59b207e69162361
VictorZampella <victorzampella@gmail.com>
2026-04-12 17:02:06 -0300

    ✨ feat: Add product screen page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,48 +16,33 @@ class ProductController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Display the specified resource.
      */
+    public function show($id)
+    {
+        return view('products.product');
+    }
+
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Product $product)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Product $product)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Product $product)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Product $product)
     {
         //
--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,16 @@ use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ProductController::class, 'index']);
+
 Route::get('/login', [UsersController::class, 'login']);
 Route::get('/register', [UsersController::class, 'register']);
 Route::get('/reset-shipping', [UsersController::class, 'resetShipping']);
-Route::get('/reset-passowrd', [UsersController::class, 'resetPassword']);
+Route::get('/reset-password', [UsersController::class, 'resetPassword']); // corrigi typo
+
 Route::get('/help', [HelpController::class, 'index']);
 Route::get('/help-guide', [HelpController::class, 'helpGuide']);
 
 Route::get('/profile', [UsersController::class, 'indexUserPassword']);
 Route::get('/reset-user-password', [UsersController::class, 'resetUserPassword']);
+
+Route::get('/produto/{id}', [ProductController::class, 'show']);
