344632eb00eae3c4b5391d97f7b36f736f17c219
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-04-15 20:17:07 -0300

    🎨 art: Add styles in help page

--- resources/views/help/index.blade.php
+++ resources/views/help/index.blade.php
@@ -9,7 +9,7 @@
     </a>
   </header>
 
-  <main class="flex flex-col justify-center items-center">
+  <main class="flex flex-col justify-center items-center gap-16">
     <header class="text-center">
       <h1 class="text-4xl">Central de Ajuda</h1>
       <span class="h-2 w-8 bg-gray-200"></span>
@@ -17,7 +17,7 @@
 
     <section class="flex flex-col gap-4">
       <button
-        class="group bg-pink-500 text-white flex items-center justify-center rounded-sm w-full pt-3 pb-3 gap-2 border-2 border-transparent hover:bg-white hover:border-2 hover:border-pink-600 hover:text-pink-600 cursor-pointer text-center outline-none transition-all duration-200 ">
+        class="group bg-pink-500 text-white flex items-center justify-center rounded-sm w-full pt-3 pb-3 pl-12 pr-12 gap-2 border-2 border-transparent hover:bg-white hover:border-2 hover:border-pink-600 hover:text-pink-600 cursor-pointer text-center outline-none transition-all duration-200 ">
         <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
           className="size-6">
           <path fillRule="evenodd"
@@ -28,7 +28,7 @@
       </button>
 
       <button
-        class="group bg-pink-500 text-white flex items-center justify-center rounded-sm w-full pt-3 pb-3 gap-2 border-2 border-transparent hover:bg-white hover:border-2 hover:border-pink-600 hover:text-pink-600 cursor-pointer text-center outline-none transition-all duration-200 ">
+        class="group bg-pink-500 text-white flex items-center justify-center rounded-sm w-full pt-3 pb-3 pl-12 pr-12 gap-2 border-2 border-transparent hover:bg-white hover:border-2 hover:border-pink-600 hover:text-pink-600 cursor-pointer text-center outline-none transition-all duration-200 ">
         <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
           className="size-6">
           <path fillRule="evenodd"
@@ -39,7 +39,7 @@
       </button>
 
       <button
-        class="group bg-pink-500 text-white flex items-center justify-center rounded-sm w-full pt-3 pb-3 gap-2 border-2 border-transparent hover:bg-white hover:border-2 hover:border-pink-600 hover:text-pink-600 cursor-pointer text-center outline-none transition-all duration-200 ">
+        class="group bg-pink-500 text-white flex items-center justify-center rounded-sm w-full pt-3 pb-3 pl-12 pr-12 gap-2 border-2 border-transparent hover:bg-white hover:border-2 hover:border-pink-600 hover:text-pink-600 cursor-pointer text-center outline-none transition-all duration-200 ">
         <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
           className="size-6">
           <path fillRule="evenodd"
@@ -50,11 +50,11 @@
       </button>
     </section>
 
-    <section>
-      <div class="flex gap-4">
-        <span class="h-2 w-8 bg-gray-200"></span>
-        <h2 class="text-2xl">Suporte</h2>
-        <span class="h-2 w-8 bg-gray-200"></span>
+    <section class="flex flex-col flex-1 gap-2">
+      <div class="flex gap-4 items-center mb-4 justify-center text-center">
+        <span class="h-0.5 w-24 bg-gray-300"></span>
+        <h2 class="text-2xl text-gray-400">Suporte</h2>
+        <span class="h-0.5 w-24 bg-gray-300"></span>
       </div>
 
       <button
@@ -68,11 +68,11 @@
         <span>Termos de Serviço?</span>
       </button>
 
-      <span class="h-2 w-full bg-gray-200"></span>
+      <span class="h-0.5 w-full mt-12 mb-12 bg-gray-300"></span>
 
-      <p>
+      <p class="flex text-lg  text-center gap-2 items-center justify-center">
         Já Tem uma Conta?
-        <a href="" class="text-blue-500 decoration-amber-400">
+        <a href="" class="text-blue-500 hover:decoration-blue-500 hover:underline">
           Login
         </a>
       </p>
