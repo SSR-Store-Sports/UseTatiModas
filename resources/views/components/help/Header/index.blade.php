c70e5b9869a5462a5f5d7a10c49b9a1b240c12a9
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-04-15 21:05:35 -0300

    🎨 art: Add adjust in help and new styles for help guide page

---
+++ resources/views/components/help/Header/index.blade.php
@@ -0,0 +1,5 @@
+<header class="flex h-28 items-center px-12 border-b-2 border-pink-500 mb-8">
+  <a class="group text-pink-600" href="/login">
+    <x-heroicon-o-arrow-left class="h-6 w-6 group-hover:text-pink-300" />
+  </a>
+</header>
