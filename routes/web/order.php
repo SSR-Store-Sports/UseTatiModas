a10bc9e72e20bfdb88684b0d48e531068597a81f
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-06-03 13:41:32 -0300

    ✨ feat: Add adjust at order display

--- routes/web/order.php
+++ routes/web/order.php
@@ -3,6 +3,6 @@ use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->controller(OrderController::class)->group(function () {
-    Route::get('/', 'index')->name('orders.index');
-    Route::get('/{id}', 'show')->name('orders.show');
+    Route::get('/', 'index')->name('index');
+    Route::get('/{id}', 'show')->name('show');
 });
