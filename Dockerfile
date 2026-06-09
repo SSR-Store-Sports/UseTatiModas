fb8e3ce99bb760cd66129724ccde650a558166e2
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-06-09 18:08:40 -0300

    ✨ feat: Add FollowSymLinks

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
-
 FROM node:20-alpine AS frontend-builder
 WORKDIR /app
 RUN npm install -g pnpm
@@ -27,6 +26,13 @@ ENV APACHE_DOCUMENT_ROOT=/var/www/html/public
 RUN sed -ri -e 's!/var/www/html!${APACHE_DOCUMENT_ROOT}!g' /etc/apache2/sites-available/*.conf
 RUN sed -ri -e 's!/var/www/html!${APACHE_DOCUMENT_ROOT}!g' /etc/apache2/apache2.conf /etc/apache2/conf-available/*.conf
 RUN a2enmod rewrite
+
+# CONFIGURAÇÃO ADICIONADA: Permite ao Apache seguir links simbólicos na pasta public
+RUN echo '<Directory /var/www/html/public>\n\
+    Options Indexes FollowSymLinks\n\
+    AllowOverride All\n\
+    Require all granted\n\
+</Directory>' >> /etc/apache2/apache2.conf
 
 WORKDIR /var/www/html
 COPY . .
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-# 1. Cria o link simbólico da pasta storage
+mkdir -p /var/www/html/storage/app/public/products
+
+rm -f /var/www/html/public/storage
+
 echo "Criando link simbólico do storage..."
 php artisan storage:link --force
 
-# 2. Garante que os arquivos enviados pelo controller ganhem a permissão correta de leitura
 echo "Ajustando permissões de uploads..."
+chown -R www-data:www-data /var/www/html/storage /var/www/html/public/storage
 chmod -R 755 /var/www/html/storage/app/public
 
-# 3. Limpa caches antigos de view e configurações
 php artisan config:clear
 php artisan view:clear
 
-# 4. Inicia o servidor Apache
+# 6. Inicia o servidor Apache
 echo "Iniciando o Apache..."
 exec apache2-foreground
