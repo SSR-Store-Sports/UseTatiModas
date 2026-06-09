3d000e59aa1e7dc3f0ce1b85b7ab02cacddb2ef3
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-06-09 17:58:53 -0300

    🐍 fix: Add storage images links

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-# Roda as migrations no banco de dados externo conectado
-echo "Rodando as migrations no banco de dados..."
-php artisan migrate --force
+# 1. Cria o link simbólico da pasta storage
+echo "Criando link simbólico do storage..."
+php artisan storage:link --force
 
-# Inicia o servidor Apache
+# 2. Garante que os arquivos enviados pelo controller ganhem a permissão correta de leitura
+echo "Ajustando permissões de uploads..."
+chmod -R 755 /var/www/html/storage/app/public
+
+# 3. Limpa caches antigos de view e configurações
+php artisan config:clear
+php artisan view:clear
+
+# 4. Inicia o servidor Apache
 echo "Iniciando o Apache..."
 exec apache2-foreground
